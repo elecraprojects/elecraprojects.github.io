8ba963957b20b6c2a0b67efa9f3b758ecfd43d96
elecraprojects <elecraproject@gmail.com>
2026-07-15 01:09:22 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("Elecra Projects Website Loaded");
+document.querySelectorAll('.card').forEach(c=>{
+c.addEventListener('mouseenter',()=>c.style.background='#e8f4ff');
+c.addEventListener('mouseleave',()=>c.style.background='#fff');
+});
+console.log('Elecra Projects Website Loaded');
